baaa0d34bb649f3970596ab7eb1ad4586196014c
Andrew Bonas <aabonas@hotmail.com>
2021-01-29 15:08:03 -0800

    form styling and click function added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import loadPage from './modules/initial-load';
 
 init();
 
-function openForm() {
+
+
+
+window.openForm = function() {
   document.getElementById("form-container").style.display = "block";
 }
-
-function closeForm() {
+window.closeForm = function() {
   document.getElementById("form-container").style.display = "none";
 }
 
 function init() {
+
   loadPage();
 }
